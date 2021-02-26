bca976a968b6e1f7b22858862d07edb68b17b56c
tameemshahriar <you@example.com>
2021-02-27 00:44:22 +0600

    ema john simple ecommerce

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+const Shop = () => {
+    // console.log(fakeData);
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) =>{
+        console.log('Product added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+    return (
+        <div className="shop-container">
+            <div className="product-container"> 
+        { 
+            products.map(pd => <Product 
+            handleAddProduct = {handleAddProduct}
+            product={pd}>
+            
+            product = {pd}
+            </Product>)
+        }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+                {/*  <h1>This is cart</h1>
+                 <h5>Order Summary: {cart.length}</h5> */}
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
